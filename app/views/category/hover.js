9f4a269c9e681cbb1df6cac7739c7f79ea6fa216
ryanleeallred <ryanleeallred@gmail.com>
2014-09-26 14:24:05 -0600

    Category page first draft is finished

---
+++ app/views/category/hover.js
@@ -0,0 +1,142 @@
+$(document).ready(function() {
+
+	//console.log("READY!!!");
+
+
+$('.category-image1').mouseenter(function(){
+	$('.image-hover1').fadeIn('fast');
+});
+$('.category-image1').mouseleave(function(){
+	$('.image-hover1').fadeOut('fast');
+});
+
+$('.category-image2').mouseenter(function(){
+	$('.image-hover2').fadeIn('fast');
+});
+$('.category-image2').mouseleave(function(){
+	$('.image-hover2').fadeOut('fast');
+});
+
+$('.category-image3').mouseenter(function(){
+	$('.image-hover3').fadeIn('fast');
+});
+$('.category-image3').mouseleave(function(){
+	$('.image-hover3').fadeOut('fast');
+});
+
+$('.category-image4').mouseenter(function(){
+	$('.image-hover4').fadeIn('fast');
+});
+$('.category-image4').mouseleave(function(){
+	$('.image-hover4').fadeOut('fast');
+});
+
+$('.category-image5').mouseenter(function(){
+	$('.image-hover5').fadeIn('fast');
+});
+$('.category-image5').mouseleave(function(){
+	$('.image-hover5').fadeOut('fast');
+});
+
+$('.category-image6').mouseenter(function(){
+	$('.image-hover6').fadeIn('fast');
+});
+$('.category-image6').mouseleave(function(){
+	$('.image-hover6').fadeOut('fast');
+});
+
+$('.category-image7').mouseenter(function(){
+	$('.image-hover7').fadeIn('fast');
+});
+$('.category-image7').mouseleave(function(){
+	$('.image-hover7').fadeOut('fast');
+});
+
+$('.category-image8').mouseenter(function(){
+	$('.image-hover8').fadeIn('fast');
+});
+$('.category-image8').mouseleave(function(){
+	$('.image-hover8').fadeOut('fast');
+});
+
+$('.category-image9').mouseenter(function(){
+	$('.image-hover9').fadeIn('fast');
+});
+$('.category-image9').mouseleave(function(){
+	$('.image-hover9').fadeOut('fast');
+});
+
+$('.category-image10').mouseenter(function(){
+	$('.image-hover10').fadeIn('fast');
+});
+$('.category-image10').mouseleave(function(){
+	$('.image-hover10').fadeOut('fast');
+});
+
+$('.category-image11').mouseenter(function(){
+	$('.image-hover11').fadeIn('fast');
+});
+$('.category-image11').mouseleave(function(){
+	$('.image-hover11').fadeOut('fast');
+});
+
+$('.category-image12').mouseenter(function(){
+	$('.image-hover12').fadeIn('fast');
+});
+$('.category-image12').mouseleave(function(){
+	$('.image-hover12').fadeOut('fast');
+});
+
+$('.category-image13').mouseenter(function(){
+	$('.image-hover13').fadeIn('fast');
+});
+$('.category-image13').mouseleave(function(){
+	$('.image-hover13').fadeOut('fast');
+});
+
+$('.category-image14').mouseenter(function(){
+	$('.image-hover14').fadeIn('fast');
+});
+$('.category-image14').mouseleave(function(){
+	$('.image-hover14').fadeOut('fast');
+});
+
+$('.category-image15').mouseenter(function(){
+	$('.image-hover15').fadeIn('fast');
+});
+$('.category-image15').mouseleave(function(){
+	$('.image-hover15').fadeOut('fast');
+});
+
+$('.category-image16').mouseenter(function(){
+	$('.image-hover16').fadeIn('fast');
+});
+$('.category-image16').mouseleave(function(){
+	$('.image-hover16').fadeOut('fast');
+});
+
+$('.category-image17').mouseenter(function(){
+	$('.image-hover17').fadeIn('fast');
+});
+$('.category-image17').mouseleave(function(){
+	$('.image-hover17').fadeOut('fast');
+});
+
+$('.category-image18').mouseenter(function(){
+	$('.image-hover18').fadeIn('fast');
+});
+$('.category-image18').mouseleave(function(){
+	$('.image-hover18').fadeOut('fast');
+});
+
+$('.category-image19').mouseenter(function(){
+	$('.image-hover19').fadeIn('fast');
+});
+$('.category-image19').mouseleave(function(){
+	$('.image-hover19').fadeOut('fast');
+});
+
+
+
+/******* End of Document *******/
+});
