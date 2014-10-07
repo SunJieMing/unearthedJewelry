5b86952ca9e2383729534dfdf64200c9d16faaf4
Ben Nelson <s.benjamin.nelson@gmail.com>
2014-10-06 18:27:06 -0700

    Fixed carousel

--- app/views/main/main.js
+++ app/views/main/main.js
@@ -8,3 +8,15 @@ angular.module('unearthedApp')
     controller: 'MainCtrl'
   });
 });
+
+//This reinitializes the carousel and eliminates several erratic errors.
+//Found the answer to this issue on SO: 
+//http://stackoverflow.com/questions/20137900/controller-carousel-required-by-directive-ngtransclude-cant-be-found
+
+angular.module('ui.bootstrap.carousel', ['ui.bootstrap.transition'])
+    .controller('CarouselController', ['$scope', '$timeout', '$transition', '$q', function        ($scope, $timeout, $transition, $q) {
+}]).directive('carousel', [function() {
+    return {
+
+    }
+}]);
