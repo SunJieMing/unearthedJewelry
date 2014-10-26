655807adcbdb76f4e9ad0cb4470fe7696d1c2368
Ben Nelson <s.benjamin.nelson@gmail.com>
2014-10-26 01:38:08 -0600

    Fixed main page carousel.  It no longer duplicates or collapses.  The carousel code in main.js should be cleaned up though.  Some of the data should be refactored into a factory/service.

--- app/views/main/main.js
+++ app/views/main/main.js
@@ -13,10 +13,26 @@ angular.module('unearthedApp')
 //Found the answer to this issue on SO: 
 //http://stackoverflow.com/questions/20137900/controller-carousel-required-by-directive-ngtransclude-cant-be-found
 
-angular.module('ui.bootstrap.carousel', ['ui.bootstrap.transition'])
-    .controller('CarouselController', ['$scope', '$timeout', '$transition', '$q', function        ($scope, $timeout, $transition, $q) {
-}]).directive('carousel', [function() {
-    return {
+// angular.module('ui.bootstrap.carousel', ['ui.bootstrap.transition'])
+//     .controller('CarouselController', ['$scope', '$timeout', '$transition', '$q', function        ($scope, $timeout, $transition, $q) {
+// }]).directive('carousel', [function() {
+//     return {
 
-    }
-}]);
+//     }
+// }]);
+
+angular.module('unearthedApp').controller('CarouselDemoCtrl', function ($scope) {
+  $scope.myInterval = 5000;
+  var slides = $scope.slides = [];
+  $scope.addSlide = function() {
+    var newWidth = 600 + slides.length;
+    slides.push({
+      image: 'images/homepage.png',
+      title: 'DZI BEAD BRACELETS',
+      description: 'The elegant design of these bracelets is unmatched this side of India. The elegant design of these bracelets is unmatched this side of India. The elegant design of these bracelets is unmatched this side of India. The elegant design of these bracelets is unmatched this side of India. The elegant design of these bracelets is unmatched this side of India. The elegant design of these bracelets is unmatched this side of India.'
+    });
+  };
+  for (var i = 0; i < 3; i++) {
+    $scope.addSlide();
+  }
+});
