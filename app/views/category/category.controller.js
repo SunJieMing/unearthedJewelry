202ee10a526aa9d39affad849bdcc1b47c4436e7
ryanleeallred <ryanleeallred@gmail.com>
2014-10-31 17:45:21 -0600

    adds hover to category items - shows hover on all items still.

--- app/views/category/category.controller.js
+++ app/views/category/category.controller.js
@@ -8,4 +8,26 @@ angular.module('unearthedApp')
     console.log($scope.braceletList);
   });
 
-}]);
+  $scope.coverToggle = false;
+
+  $scope.reveal = function () {
+  	$scope.coverToggle = true;
+  	console.log('showing');
+  }
+
+  $scope.hide = function () {
+  	$scope.coverToggle = false;
+  	console.log('hiding');
+  }
+
+}])
+
+
+// .directive('productHover', function() {
+//  	return {
+
+//  		template:'<h2>{{product.name}}</h2><p>{{product.mainDescription}}</p>'
+
+//  	};
+
+//  });
