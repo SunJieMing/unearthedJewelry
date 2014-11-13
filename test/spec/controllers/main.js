41b6eb11243690fa2a1aa9fde2f46967ed0d9c7a
Ben Nelson <s.benjamin.nelson@gmail.com>
2014-11-13 00:04:29 -0700

    Added unit testing for main.js carousel.

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -6,7 +6,7 @@ describe('Controller: MainCtrl', function () {
   beforeEach(module('unearthedApp'));
 
   var MainCtrl,
-    scope;
+  scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
@@ -19,5 +19,40 @@ describe('Controller: MainCtrl', function () {
   it('should have a hello world test', function () {
     expect(scope.test).toBe('Hello World!');
   });
+
+});
+
+describe('Controller: CarouselDemoCtrl', function () {
+
+  beforeEach(module('unearthedApp'));
+
+  var MainCtrl,
+  scope;
+
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    MainCtrl = $controller('CarouselDemoCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('interval should be initialized to 5000ms', function() {
+    expect(scope.myInterval).toBe(5000);
+  });
+
+  it('should have a method addSlide', function() {
+    expect(scope.addSlide).not.toBe(undefined);
+  });
+
+  it('carousel array should have a length of three', function() {
+    expect(scope.slides.length).toBe(3);
+  })  
+
+  it('first carousel image is not properly set', function() {
+    var firstItem = scope.slides[0];
+    expect(firstItem.image).toBe('images/homepage.png');
+    expect(firstItem.title).toBe('DZI BEAD BRACELETS');
+    expect(firstItem.description).toBe('The elegant design of these bracelets is unmatched this side of India. The elegant design of these bracelets is unmatched this side of India. The elegant design of these bracelets is unmatched this side of India. The elegant design of these bracelets is unmatched this side of India. The elegant design of these bracelets is unmatched this side of India. The elegant design of these bracelets is unmatched this side of India.');
+  })
 
 });
