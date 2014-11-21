d76b4b41438eb9de49ed399c46850ad2c2189103
Ben Nelson <s.benjamin.nelson@gmail.com>
2014-11-21 16:28:25 -0700

    Added factory to main view for dynamically loading main view's copy.

---
+++ app/views/main/main.factory.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('unearthedApp')
+.factory('MainFactory', function ($q, $http) {
+
+  var getPageCopy = function(){
+    var deferred = $q.defer();
+
+    $http({
+      method: 'GET',
+      url: 'https://api.parse.com/1/classes/mainCopy',
+    }).success(function(info, status){
+          // console.log('info', info, 'status', status);
+          deferred.resolve(info);
+          // return info;
+        }).error(function(info, status){
+          console.error('GET Request fails with the following...',  info, status);
+          deferred.reject('Error', info, 'Status', status);
+          // console.log('Error', info, 'Status', status);
+        });
+        return deferred.promise;
+      }
+
+      return {
+        getPageCopy: getPageCopy
+      };
+    });
