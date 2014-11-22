7a5f76971851536ec72cc9bee853f12559219091
Ben Nelson <s.benjamin.nelson@gmail.com>
2014-11-22 07:29:36 -0700

    Added image selection for product view.

--- app/views/product/product.controller.js
+++ app/views/product/product.controller.js
@@ -2,9 +2,14 @@
 angular.module('unearthedApp')
 .controller('ProductCtrl', ['$scope', 'ProductFactory', function($scope, ProductFactory) {  
   $scope.individualItem = {};
-  
+  $scope.mainImageUrl = '';
   ProductFactory.getItem().then(function(data){
     $scope.individualItem = data;
+    $scope.mainImageUrl = data.mainImage.url;
   });
+
+  $scope.switchImage = function(img) {
+    $scope.mainImageUrl = img;
+  }
 
 }]);
