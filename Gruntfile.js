1742851077cb08f470675d17e6139364fca8166b
Ryan Allred <ryanleeallred@gmail.com>
2014-12-09 11:02:11 -0700

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,7 +66,7 @@ module.exports = function (grunt) {
       options: {
         port: 80,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'http://104.131.86.186/',
+        hostname: '104.131.86.186',
         livereload: 35729
       },
       livereload: {
